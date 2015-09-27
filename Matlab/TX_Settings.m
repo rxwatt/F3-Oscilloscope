8b8992d01b1cc1272708e1c2de6e16574b718ccc
rxwatt <a2temp@mail.com>
2015-09-27 10:37:22 +0200

    Update TX_Settings.m

--- Matlab/TX_Settings.m
+++ Matlab/TX_Settings.m
@@ -1,4 +1,6 @@
 
+% octave-compatible
+% @rxwatt
 command_string = zeros(8,1);
 command_string(1) = '$';
 command_string(2) = 'O';
@@ -9,4 +11,6 @@ command_string(6) = trigger_level;
 command_string(7) = trigger_type;
 command_string(8) = trigger_position;
 command_string = uint8(command_string);
-fwrite(com,command_string);
+%fwrite(com,command_string);
+%*** Line ABOVE works for MATLAB. Linr BELOW works for OCTAVE ***%
+srl_write(com,command_string);
